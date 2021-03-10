7c546c444db3013022913cdda663e06cfd1834ef
enescakr55 <enescakr01@hotmail.com>
2021-03-10 22:45:46 +0300

    master

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,12 +9,14 @@ import {HttpClient} from '@angular/common/http';
 export class ProductComponent implements OnInit {
 
   products:Product[] = []
-  constructor(httpClient:HttpClient) { }
+  apiUrl = "https://localhost:44314/api/products/getall";
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
     console.log("İnit çalıştı");
   }
   getProducts(){
+    this.httpClient.get(this.apiUrl);
 
   }
 
