873af07cb1a38a51e0566e2f14c056e141bedc22
enescakr55 <enescakr01@hotmail.com>
2021-03-18 00:02:48 +0300

    18.03.2021 -- Sepet işlemleri , Site içi toast notification , ürün arama eklendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -4,6 +4,8 @@ import {HttpClient} from '@angular/common/http';
 import { ProductService } from 'src/app/services/product.service';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -20,7 +22,8 @@ export class ProductComponent implements OnInit {
     message:"",
     success:true
   }
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) { }
+  filterText="";
+  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute,private toastrService:ToastrService,private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -44,7 +47,10 @@ export class ProductComponent implements OnInit {
       this.dataLoaded = true;
     })
   }
-
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    this.toastrService.success("Sepete eklendi",product.productName)
+  }
 
 
 }
